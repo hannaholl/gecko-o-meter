9c266dbc1dff12325d59a83bae61ae052cbed25b
Hanna Hollstrom <hanna.nygardhollstrom@timeinc.com>
2016-10-18 18:55:14 +0100

    Initial commit

---
+++ src/components/MeterContainer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class DemoContainer extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="meter-container">
+
+      </div>
+    );
+  }
+}
+
+export default DemoContainer;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MeterContainer from './components/MeterContainer';
+
+ReactDOM.render((
+  <MeterContainer />
+), document.getElementById('container'));
